3b2266243e2eee16606d0dd2af1a55277da7f1dc
Chance Martin <chancemartin116@gmail.com>
2022-01-11 15:41:54 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Box, Grid, ResponsiveContext } from 'grommet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grommet plain full>
+      <ResponsiveContext.Consumer>
+      {size => size === "small" ? ( 
+        <Grid
+          fill={true}
+          rows={['20%', '20%', '20%', '20%', '20%']}
+          columns={['auto']}
+          areas={[
+            { name: 'header', start: [0, 0], end: [0, 0] },
+            { name: 'nav', start: [0, 1], end: [0, 1] },
+            { name: 'sidebar', start: [0, 2], end: [0, 2] },
+            { name: 'content', start: [0, 3], end: [0, 3] },
+            { name: 'footer', start: [0, 4], end: [0, 4]},
+          ]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box gridArea="header" background="blue">header</Box>
+          <Box gridArea="nav" background="orange">nav</Box>
+          <Box gridArea="sidebar" background="green">sidebar</Box>
+          <Box gridArea="content" background="red">content</Box>
+          <Box gridArea="footer" background="yellow">footer</Box>
+        </Grid> ) :
+        (<Grid
+          fill={true}
+          rows={['20%', 'auto', '20%']}
+          columns={['10%', 'auto', '10%']}
+          areas={[
+            { name: 'header', start: [0, 0], end: [2, 0] },
+            { name: 'nav', start: [0, 1], end: [0, 2] },
+            { name: 'content', start: [1, 1], end: [2, 2] },
+            { name: 'footer', start: [1, 2], end: [2, 2]},
+            { name: 'sidebar', start: [2, 1], end: [2, 1] },
+          ]}
+        >
+          <Box gridArea="header" background="blue">header</Box>
+          <Box gridArea="nav" background="orange">nav</Box>
+          <Box gridArea="sidebar" background="green">sidebar</Box>
+          <Box gridArea="content" background="red">content</Box>
+          <Box gridArea="footer" background="yellow">footer</Box>
+        </Grid>)}
+        </ResponsiveContext.Consumer>
+    </Grommet>
   );
 }
 
