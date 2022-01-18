3e396f4e1e663cf12d027a935774469f52230a84
Chance Martin <chancemartin116@gmail.com>
2022-01-18 11:40:48 -0800

    optimize and fix for BigInts

--- hashApplicationProblem.js
+++ hashApplicationProblem.js
@@ -1,29 +1,59 @@
 const CHARACTERS = "acdefgilnoprstuw";
-var hash = parseInt(process.argv[2]);
-var numberOfLetters = parseInt(process.argv[3]);
-var output = new Array(numberOfLetters);
-printStringForHashAndNumberOfLetters(output, hash, numberOfLetters);
+var hash = 1317985395605002507n; // westernista
+//hash = 934605031880300n; // leapfrogs
+//hash = 677842960118n; // gleeful
+printStringForHashAndCount(hash, 11);
+
+function printStringForHashAndCount(hash, count) {
+    let start = [];
+    for(let i = 0; i < count; i++) {
+        start.push(CHARACTERS[0]);
+    }
+    let k = 0;
+    let j = 0
+    while (k < count) {
+        j = 0;
+        while(j < CHARACTERS.length) {
+            start[k] = CHARACTERS[j]
+            let string = start.join("")
+            let currentHash = getHashForString(string);
+            if (currentHash === hash) {
+                console.log(string);
+                return;
+            } else if (currentHash > hash) {
+                start[k] = CHARACTERS[j - 1];
+                k++;
+                j = 0;
+            } else {
+                j++;
+            }
+        }
+        k++;
+    }
+}
+
+function getHashForString(s) {
+    let h = 7n;
+    for(var i = 0; i < s.length; i++) {
+        h = h * 37n + BigInt(CHARACTERS.indexOf(s.charAt(i)));
+    }
+    return h;
+}
 
 function printStringForHashAndNumberOfLetters(output, hash, numberOfLetters) {
+    let str = output.join("")
+    let currentHash = getHashForString(str)
     if (numberOfLetters === 0) {
-        var str = output.join("")
-        if (getHashForString(str) === hash) {
-            console.log(str);
+        console.log(str);
+        if (currentHash === hash) {
+            console.log("here")
             return;
         }
     } else {
-        for(var i = 0; i < CHARACTERS.length; i++) {
+        for(let i = 0; i < CHARACTERS.length; i++) {
             output.push(CHARACTERS.charAt(i));
             printStringForHashAndNumberOfLetters(output, hash, numberOfLetters - 1);
             output.pop();
         }
     }
-}
-
-function getHashForString(s) {
-    var h = 7;
-    for(var i = 0; i < s.length; i++) {
-        h = h * 37 + CHARACTERS.indexOf(s.charAt(i));
-    }
-    return h;
 }
