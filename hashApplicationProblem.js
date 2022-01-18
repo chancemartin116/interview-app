7cea7667a8e543bbfc185af35d91720ba8081ef0
Chance Martin <chancemartin116@gmail.com>
2022-01-18 13:22:14 -0800

    refactor

--- hashApplicationProblem.js
+++ hashApplicationProblem.js
@@ -1,39 +1,41 @@
-const CHARACTERS = "acdefgilnoprstuw";
-var hash = 1317985395605002507n; // westernista
-//hash = 934605031880300n; // leapfrogs
-//hash = 677842960118n; // gleeful
-printStringForHashAndCount(hash, 11);
+class HashApplicationProblem {
+    static #CHARACTERS = "acdefgilnoprstuw";
 
-function printStringForHashAndCount(hash, count) {
-    let currentVariation = getFirstVariationForCount(count);
-    for(let variationIndex = 0; variationIndex < currentVariation.length; variationIndex++) {
-        for(let charactersIndex = 0; charactersIndex < CHARACTERS.length; charactersIndex++) {
-            currentVariation[variationIndex] = CHARACTERS[charactersIndex]
-            let currentString = currentVariation.join("")
-            let currentHash = getHashForString(currentString);
-            if (currentHash === hash) {
-                console.log(currentString);
-                return;
-            } else if (currentHash > hash) {
-                currentVariation[variationIndex] = CHARACTERS[charactersIndex - 1];
-                break;
-            }                 
-        }    
+    // Get string from hash and number of letters (count)
+    // Runs in O(S^2) ?
+    static printStringForHashAndCount(hash, count) {
+        let currentVariation = HashApplicationProblem.#getFirstVariationForCount(count);
+        for(let variationIndex = 0; variationIndex < currentVariation.length; variationIndex++) {
+            for(let charactersIndex = 0; charactersIndex < HashApplicationProblem.#CHARACTERS.length; charactersIndex++) {
+                currentVariation[variationIndex] = HashApplicationProblem.#CHARACTERS[charactersIndex]
+                let currentString = currentVariation.join("")
+                let currentHash = HashApplicationProblem.#getHashForString(currentString);
+                if (currentHash === hash) {
+                    console.log(currentString);
+                    return;
+                } else if (currentHash > hash) {
+                    currentVariation[variationIndex] = HashApplicationProblem.#CHARACTERS[charactersIndex - 1];
+                    break;
+                }                 
+            }    
+        }
+    }
+
+    static #getFirstVariationForCount(count) {
+        let firstVariation = [];
+        for(let i = 0; i < count; i++) {
+            firstVariation.push(HashApplicationProblem.#CHARACTERS[0]);
+        }
+        return firstVariation;
+    }
+
+    static #getHashForString(s) {
+        let h = 7n;
+        for(var i = 0; i < s.length; i++) {
+            h = h * 37n + BigInt(HashApplicationProblem.#CHARACTERS.indexOf(s.charAt(i)));
+        }
+        return h;
     }
 }
 
-function getFirstVariationForCount(count) {
-    let firstVariation = [];
-    for(let i = 0; i < count; i++) {
-        firstVariation.push(CHARACTERS[0]);
-    }
-    return firstVariation;
-}
-
-function getHashForString(s) {
-    let h = 7n;
-    for(var i = 0; i < s.length; i++) {
-        h = h * 37n + BigInt(CHARACTERS.indexOf(s.charAt(i)));
-    }
-    return h;
-}
+HashApplicationProblem.printStringForHashAndCount(BigInt(process.argv[2]), process.argv[3])
